fdfdf9a6c8deeec4cc3de5766bfe941ccd5dd521
Ahmed Ramadan El_Malky <Ahmed.RamadanCS21@fcai.usc.edu.eg>
2024-10-02 10:06:52 +0300

    init the project , prepare themes and colors

---
+++ app/src/main/java/com/example/go_orders/GoOrdersApp.kt
@@ -0,0 +1,16 @@
+package com.example.go_orders
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.go_orders.ui.theme.GoOrdersTheme
+
+@Composable
+fun GoOrdersApp(){
+    GoOrdersTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/go_orders/screens/HomeScreen.kt
@@ -0,0 +1,18 @@
+package com.example.go_orders.screens
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun HomeScreen(){
+
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun PreviewHomeScreen(){
+    HomeScreen()
+}
